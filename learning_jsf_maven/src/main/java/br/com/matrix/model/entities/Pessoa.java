0280cb07c15babdcbfbc1b7420e6d99566d08741
fernando.osawa <fernando.osawa@192.168.0.88>
2013-06-11 16:00:01 -0300

    Finalização das entidades.

--- learning_jsf_maven/src/main/java/br/com/matrix/model/entities/Pessoa.java
+++ learning_jsf_maven/src/main/java/br/com/matrix/model/entities/Pessoa.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import org.hibernate.annotations.ForeignKey;
@@ -37,9 +40,14 @@ public class Pessoa implements Serializable
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataDeCadastro;
 
+    @OneToOne(mappedBy = "pessoa", fetch = FetchType.LAZY)
+    @ForeignKey(name = "EndereçoPessoa")
+    private Endereço endereço;
+    
     @ManyToOne(optional = false)
     @ForeignKey(name = "PessoaSexo")
-    private Pessoa pessoa;
+    @JoinColumn(name = "IdSexo", referencedColumnName = "IdSexo")
+    private Sexo sexo;
     
     public Pessoa() 
     {
@@ -144,6 +152,36 @@ public class Pessoa implements Serializable
     
     }
 
+    public Sexo getSexo() 
+    {
+    
+        return sexo;
+    
+    }
+
+    public void setSexo(Sexo sexo) 
+    {
+    
+        this.sexo = sexo;
+    
+    }
+
+    public Endereço getEndereço() 
+    {
+    
+        return endereço;
+    
+    }
+
+    public void setEndereço(Endereço endereço) 
+    {
+    
+        this.endereço = endereço;
+    
+    }
+
+    
+    
     @Override
     public int hashCode()
     {
