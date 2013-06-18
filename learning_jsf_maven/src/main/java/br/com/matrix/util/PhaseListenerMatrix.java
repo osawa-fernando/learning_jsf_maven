cf64df84d9bd478c7e9315040e3547a1c52b20e5
fernando.osawa <fernando.osawa@192.168.0.88>
2013-06-18 14:07:04 -0300

    Resolvendo bugs e criando o template - 18/06/2013

--- learning_jsf_maven/src/main/java/br/com/matrix/util/PhaseListenerMatrix.java
+++ learning_jsf_maven/src/main/java/br/com/matrix/util/PhaseListenerMatrix.java
@@ -11,11 +11,10 @@ public class PhaseListenerMatrix implements PhaseListener
     @Override
     public void beforePhase(PhaseEvent phase) 
     {
-    
+       System.out.println("Antes da fase: " + phase.getPhaseId().toString());
        if(phase.getPhaseId().equals(PhaseId.RESTORE_VIEW))
        {
-           
-           System.out.println("Antes da fase: " + getPhaseId().toString());
+                      
            Session session = HibernateUtil.getSessionFactory().openSession();
            session.beginTransaction();
            FacesContextUtil.setRequestSession(session);
@@ -27,10 +26,10 @@ public class PhaseListenerMatrix implements PhaseListener
     @Override
     public void afterPhase(PhaseEvent phase) 
     {
-    
+        System.out.println("Depois da fase: " + phase.getPhaseId());
         if(phase.getPhaseId().equals(PhaseId.RENDER_RESPONSE))
         {
-            System.out.println("Depois da fase: " + getPhaseId());
+            
             Session session = FacesContextUtil.getRequestSession();
             try 
             {
