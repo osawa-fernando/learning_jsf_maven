b6a869be928f5e6c935b43a3294e0ddb86f96dd4
fernando.osawa <fernando.osawa@192.168.0.88>
2013-06-21 14:30:12 -0300

    Criando o primero ManagedBean - 21/06/2013

--- learning_jsf_maven/src/main/java/br/com/matrix/util/PhaseListenerMatrix.java
+++ learning_jsf_maven/src/main/java/br/com/matrix/util/PhaseListenerMatrix.java
@@ -5,68 +5,40 @@ import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 import org.hibernate.Session;
 
-public class PhaseListenerMatrix implements PhaseListener
-{
+public class PhaseListenerMatrix implements PhaseListener {
 
+    //Antes da Fase
     @Override
-    public void beforePhase(PhaseEvent phase) 
-    {
-       System.out.println("Antes da fase: " + phase.getPhaseId().toString());
-       if(phase.getPhaseId().equals(PhaseId.RESTORE_VIEW))
-       {
-                      
-           Session session = HibernateUtil.getSessionFactory().openSession();
-           session.beginTransaction();
-           FacesContextUtil.setRequestSession(session);
-                      
-       }
-    
-    }
-
-    @Override
-    public void afterPhase(PhaseEvent phase) 
-    {
-        System.out.println("Depois da fase: " + phase.getPhaseId());
-        if(phase.getPhaseId().equals(PhaseId.RENDER_RESPONSE))
-        {
-            
-            Session session = FacesContextUtil.getRequestSession();
-            try 
-            {
-            
-                session.getTransaction().commit();
-                
-            } 
-            catch (Exception e) 
-            {
-                
-                if(session.getTransaction().isActive())
-                {
-                    
-                    session.getTransaction().rollback();
-                                        
-                }
-                
-            }
-            finally
-            {
-                
-                session.close();
-                
-            }
-            
+    public void beforePhase(PhaseEvent fase) {
+        System.out.println("Antes da fase: "+ fase.getPhaseId());
+        if (fase.getPhaseId().equals(PhaseId.RESTORE_VIEW)) {            
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            FacesContextUtil.setRequestSession(session);            
         }
+    }
     
+    //Depois da Fase
+    @Override
+    public void afterPhase(PhaseEvent fase) {
+        System.out.println("Depois da fase: "+ fase.getPhaseId());
+        if (fase.getPhaseId().equals(PhaseId.RENDER_RESPONSE)) {
+            Session session = FacesContextUtil.getRequestSession();
+            try {
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            } finally{
+                session.close();
+            }
+        }
     }
 
     @Override
-    public PhaseId getPhaseId() 
-    {
-    
+    public PhaseId getPhaseId() {
         return PhaseId.ANY_PHASE;
-    
     }
-    
-       
     
 }
